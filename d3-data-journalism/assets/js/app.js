2844fd5f6e2bedf27d3d0783b8bc3f01fa4a49d9
Sunwoo Kim <s.kim32415@gmail.com>
2021-07-11 19:32:16 -0700

    added dimensions for svg wrapper

--- d3-data-journalism/assets/js/app.js
+++ d3-data-journalism/assets/js/app.js
@@ -1,7 +1,26 @@
 // @TODO: YOUR CODE HERE!
+
+// Create svg wrapper
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Import data
 d3.csv("assets/data/data.csv").then(function(stateData) {
 
   console.log(stateData);
+
+  // Get scatter element with d3
+  var scatterPlot = d3.select("#scatter");
 
 }).catch(function(error) {
   console.log(error);
